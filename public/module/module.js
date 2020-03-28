0ce403b2ca1206a8107fd8862c4cd2dcaeba167d
play.js <git@gituser.com>
2020-03-29 00:21:59 +0200

    Add ui.router

--- public/module/module.js
+++ public/module/module.js
@@ -1,3 +1,27 @@
 'use strict'
 
-let app = angular.module('app', ['ngMessages']);
+let app = angular.module('app', ['ngMessages', 'ui.router']);
+
+
+app.config(function($locationProvider, $stateProvider, $urlRouterProvider) {
+   //$locationProvider.html5Mode(true);
+   $locationProvider.hashPrefix('!');
+  
+  
+  var helloState = {
+    name: 'hello',
+    url: '/hello',
+    template: '<h3>hello world!</h3>'
+  }
+
+  var aboutState = {
+    name: 'about',
+    url: '/about',
+    template: '<h3>Its the UI-Router hello world app!</h3>'
+  }
+
+  $stateProvider.state(helloState);
+  $stateProvider.state(aboutState);
+  
+  $urlRouterProvider.otherwise("/");
+});
